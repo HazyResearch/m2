2eaf8c8d9ea47dedf3a24c361bded142b6da5ae7
michaelfeil <me@michaelfeil.eu>
2024-01-12 16:52:07 +0100

    improve file path

--- .github/workflows/scripts/rename_whl_with_torch_version.py
+++ .github/workflows/scripts/rename_whl_with_torch_version.py
@@ -7,7 +7,7 @@ torch_name  = 'torch' + (torch.__version__.split('+')[0] + 'cu' + torch.version.
 file = glob.glob(os.path.join('dist', '*.whl'))
 if file:
     file = file[0]
-    insert_pos = re.search(r'cp[0-9]+-cp[0-9]+-', file).start() 
-    new_name = f'{file[:insert_pos]}+{torch_name}-{file[insert_pos:]}' 
+    insert_pos = re.search(r'-cp[0-9]+-cp[0-9]+-', file).start() 
+    new_name = f'{file[:insert_pos]}+{torch_name}{file[insert_pos:]}' 
     print(f'move whl from {file} to {new_name}') 
     shutil.move(file, new_name)
