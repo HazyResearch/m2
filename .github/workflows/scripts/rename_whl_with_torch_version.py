4de0988ec1f5dd435eaa11f7ea5495bbbc736446
michaelfeil <me@michaelfeil.eu>
2024-01-12 16:04:34 +0100

    update windows build

--- .github/workflows/scripts/rename_whl_with_torch_version.py
+++ .github/workflows/scripts/rename_whl_with_torch_version.py
@@ -1,13 +1,13 @@
 import os, shutil, glob, re
 import torch
 
-torch_name  = 'torch' + (torch.__version__.split('+')[0] + '-cu' + torch.version.cuda).replace('.', '') 
-arch_last = os.environ.get('CUDA_ARCH_LIST',';').split(';')[-1].replace('+', '') 
+torch_name  = 'torch' + (torch.__version__.split('+')[0] + 'cu' + torch.version.cuda).replace('.', '') 
+
 # get the .whl file in the dist directory and rename 
 file = glob.glob(os.path.join('dist', '*.whl'))
 if file:
     file = file[0]
     insert_pos = re.search(r'cp[0-9]+-cp[0-9]+-', file).start() 
-    new_name = f'{file[:insert_pos]}{torch_name}{arch_last}-{file[insert_pos:]}' 
+    new_name = f'{file[:insert_pos]}+{torch_name}-{file[insert_pos:]}' 
     print(f'move whl from {file} to {new_name}') 
     shutil.move(file, new_name)
